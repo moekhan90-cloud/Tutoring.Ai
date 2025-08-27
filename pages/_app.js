2bb0458a62cc1bcac134bb9920fb5b844f9a4128
moekhan90-cloud <moe.khan90@gmail.com>
2025-08-27 08:14:20 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
+// Works with the jsconfig alias:
 import '@/styles/globals.css'
 
+// Minimal Next.js App wrapper
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
