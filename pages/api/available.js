7037ac94650ce24acc20805cb0150979a4c6741f
moekhan90-cloud <moe.khan90@gmail.com>
2025-08-29 00:48:29 +0100

    Update available.js

--- pages/api/available.js
+++ pages/api/available.js
@@ -1,13 +1,38 @@
 // pages/api/available.js
-import { AVAILABLE, ALL_QUESTION_SETS } from '../../data/questions-index';
+import { AGE8_QUESTION_SETS } from '../../data/questions-age8';
+import { AGE9_QUESTION_SETS } from '../../data/questions-age9';
+import { AGE10_QUESTION_SETS } from '../../data/questions-age10';
+import { AGE11_QUESTION_SETS } from '../../data/questions-age11';
+import { AGE12_QUESTION_SETS } from '../../data/questions-age12';
+import { AGE13_QUESTION_SETS } from '../../data/questions-age13';
+import { AGE14_QUESTION_SETS } from '../../data/questions-age14';
+import { AGE15_QUESTION_SETS } from '../../data/questions-age15';
+
+const ALL = [
+  ...AGE8_QUESTION_SETS,
+  ...AGE9_QUESTION_SETS,
+  ...AGE10_QUESTION_SETS,
+  ...AGE11_QUESTION_SETS,
+  ...AGE12_QUESTION_SETS,
+  ...AGE13_QUESTION_SETS,
+  ...AGE14_QUESTION_SETS,
+  ...AGE15_QUESTION_SETS
+];
 
 export default function handler(req, res) {
+  const summary = {};
+  ALL.forEach(set => {
+    const key = `${set.age}-${set.subject}`;
+    summary[key] = set.questions.length;
+  });
+
   res.status(200).json({
-    counts: ALL_QUESTION_SETS.reduce((acc, s) => {
-      const key = `${s.age}-${s.subject}`;
-      acc[key] = s.questions.length;
+    counts: summary,
+    available: Object.keys(summary).reduce((acc, key) => {
+      const [age, subject] = key.split('-');
+      if (!acc[age]) acc[age] = [];
+      acc[age].push(subject);
       return acc;
-    }, {}),
-    available: AVAILABLE
+    }, {})
   });
 }
