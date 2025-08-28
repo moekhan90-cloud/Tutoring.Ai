8d62f8e5f35d8fdcf1c43953384c3633c6b2e5c8
moekhan90-cloud <moe.khan90@gmail.com>
2025-08-28 11:04:12 +0100

    Create questions-age10.js

---
+++ data/questions-age10.js
@@ -0,0 +1,6 @@
+// data/questions-age10.js
+export const AGE10_QUESTION_SETS = [
+  { age: 10, subject: 'Maths', questions: [] },
+  { age: 10, subject: 'English', questions: [] },
+  { age: 10, subject: 'Science', questions: [] }
+];
