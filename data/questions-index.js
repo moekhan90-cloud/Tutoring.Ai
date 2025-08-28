e25d2aa711c41d0da2d243842fd1cf4bf7c8a85a
moekhan90-cloud <moe.khan90@gmail.com>
2025-08-28 11:17:49 +0100

    Update questions-index.js

--- data/questions-index.js
+++ data/questions-index.js
@@ -1,9 +1,20 @@
 // data/questions-index.js
 import { AGE8_QUESTION_SETS } from './questions-age8';
-import { AGE9_QUESTION_SETS } from './questions-age9'; // add more imports as you create them
+import { AGE9_QUESTION_SETS } from './questions-age9';
+import { AGE10_QUESTION_SETS } from './questions-age10';
+import { AGE11_QUESTION_SETS } from './questions-age11';
+import { AGE12_QUESTION_SETS } from './questions-age12';
+import { AGE13_QUESTION_SETS } from './questions-age13';
+import { AGE14_QUESTION_SETS } from './questions-age14';
+import { AGE15_QUESTION_SETS } from './questions-age15';
 
 export const ALL_QUESTION_SETS = [
   ...AGE8_QUESTION_SETS,
   ...AGE9_QUESTION_SETS,
-  // add ...AGE10_QUESTION_SETS etc. later
+  ...AGE10_QUESTION_SETS,
+  ...AGE11_QUESTION_SETS,
+  ...AGE12_QUESTION_SETS,
+  ...AGE13_QUESTION_SETS,
+  ...AGE14_QUESTION_SETS,
+  ...AGE15_QUESTION_SETS
 ];
