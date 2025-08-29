aff30f2b128102b64590188a2d92300a373fbb1d
moekhan90-cloud <moe.khan90@gmail.com>
2025-08-29 10:32:46 +0100

    Create english.js

---
+++ pages/quiz/english.js
@@ -0,0 +1,5 @@
+// pages/quiz/english.js
+import SubjectQuizPage from '../../components/SubjectQuizPage';
+export default function EnglishPage() {
+  return <SubjectQuizPage subject="English" />;
+}
