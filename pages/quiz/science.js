f8d0f4a0552b7c660193287c6dc07c53d7e14d06
moekhan90-cloud <moe.khan90@gmail.com>
2025-08-29 10:33:32 +0100

    Create science.js

---
+++ pages/quiz/science.js
@@ -0,0 +1,5 @@
+// pages/quiz/science.js
+import SubjectQuizPage from '../../components/SubjectQuizPage';
+export default function SciencePage() {
+  return <SubjectQuizPage subject="Science" />;
+}
