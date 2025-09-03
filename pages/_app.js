30c001c71ed7ee2f38110953d3c62caa037c8139
moekhan90-cloud <moe.khan90@gmail.com>
2025-09-03 23:42:34 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,5 @@
-// pages/_app.js
-import '../styles/globals.css';
-import Head from 'next/head';
-import Header from '../components/Header.jsx';
-import { SessionProvider } from 'next-auth/react';
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <SessionProvider session={session}>
-      <Head>
-        <title>Adaptive Tutoring.ai</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Header />
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
